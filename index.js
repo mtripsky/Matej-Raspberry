1aa4fc923c605262aff848b26649c26a3ac4ec0e
mates <m.tripsky@gmail.com>
2019-09-08 16:48:17 +0200

    add html page

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => {
-  res.send('Raspberry hey')
-});
+//app.get('/', (req, res) => {
+//  res.send('Raspberry hey')
+//});
+app.use(express.static('public'))
 
 app.listen(3000, () => console.log('Server running on port 3000'))
